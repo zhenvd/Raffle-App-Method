abac96605507a7a8007317e8224d6664273e8179
Reginald Beninche <rbeninche@gmail.com>
2021-06-21 14:03:29 -0400

    V1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        //Start writing your code here
+
+
+
+
+
+
+        static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
+        {
+            var counter = 0;
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Clear();
+
+                switch (counter % 4)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("         ╔════╤╤╤╤════╗");
+                            Console.WriteLine("         ║    │││ \\   ║");
+                            Console.WriteLine("         ║    │││  O  ║");
+                            Console.WriteLine("         ║    OOO     ║");
+                            break;
+                        };
+                    case 1:
+                        {
+                            Console.WriteLine("         ╔════╤╤╤╤════╗");
+                            Console.WriteLine("         ║    ││││    ║");
+                            Console.WriteLine("         ║    ││││    ║");
+                            Console.WriteLine("         ║    OOOO    ║");
+                            break;
+                        };
+                    case 2:
+                        {
+                            Console.WriteLine("         ╔════╤╤╤╤════╗");
+                            Console.WriteLine("         ║   / │││    ║");
+                            Console.WriteLine("         ║  O  │││    ║");
+                            Console.WriteLine("         ║     OOO    ║");
+                            break;
+                        };
+                    case 3:
+                        {
+                            Console.WriteLine("         ╔════╤╤╤╤════╗");
+                            Console.WriteLine("         ║    ││││    ║");
+                            Console.WriteLine("         ║    ││││    ║");
+                            Console.WriteLine("         ║    OOOO    ║");
+                            break;
+                        };
+                }
+
+                counter++;
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
